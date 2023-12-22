ca8a3d8350f5d5abc0b50d10b12ddc7e9d6d802f
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-12-22 14:53:49 +0100

    Completato Esercizio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 /*
 
-    1) Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+    1) Stampare a schermo un messaggio all'interno di un h1, utilizzando i data. OK
 
     BONUS
     --  Aggiungere alla pagina un'immagine, presa anch'essa da un data.
 
 */
+// Dichiaro la funzione "Create App"
+const { createApp } = Vue;
+
+// Costruisco la funzione "Create App"
+createApp({
+    data() {
+        return {
+            message: 'Bellissimo Vue spero di riuscire a stampre questo messaggio in pagina'
+        }
+    }
+}).mount('h1');
